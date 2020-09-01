5f8872f34977a7999551f745bb9beff34a60362e
Opilane TTHK <opilane@tthk.ee>
2020-09-01 13:22:09 +0300

    rafael

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld!");
+            //this is a commentary
+            //Console.WriteLine("HelloWorld!");
+            Console.WriteLine("What`s your name?");
+            string Name;
+            Name = Console.ReadLine();
+            Console.WriteLine("Hello," + Name);
+
+
+        }
+    }
+}
